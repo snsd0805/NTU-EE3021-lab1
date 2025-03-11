75539326565f592bf25c6be9055b77d26fbedc1a
Ting-Jun Wang <levi900227@gmail.com>
2025-03-12 00:25:07 +0800

    feat: add FreeRTOS threads to blink LEDs respectively

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -22,7 +22,7 @@
 #define __STM32L4xx_IT_H
 
 #ifdef __cplusplus
-extern "C" {
+ extern "C" {
 #endif
 
 /* Private includes ----------------------------------------------------------*/
@@ -51,9 +51,7 @@ void HardFault_Handler(void);
 void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
-void SVC_Handler(void);
 void DebugMon_Handler(void);
-void PendSV_Handler(void);
 void SysTick_Handler(void);
 void EXTI9_5_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
